0ba1ad642e89ebdc9920ce4c228f9761542f70be
Sergey Frolov <sergey.frv@yandex.ru>
2026-01-14 11:40:25 +0600

    remove macros(q) overide key binding

--- nvim/.config/lua/plugins/dap.lua
+++ nvim/.config/lua/plugins/dap.lua
@@ -8,7 +8,7 @@ return {
       local dap = require "dap"
 
       -- Keymaps for controlling the debugger
-      vim.keymap.set("n", "q", function()
+      vim.keymap.set("n", "<leader>dq", function()
         dap.terminate()
         dap.clear_breakpoints()
       end, { desc = "Terminate and clear breakpoints" })
